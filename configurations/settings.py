95e21780cacaa8184b214de1a333917a9d2b9b88
Eugeny <evgeny.gundorov@gmail.com>
2025-07-09 19:38:09 +0300

    Refactor: move async logic to Celery task, clean up views

--- configurations/settings.py
+++ configurations/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
